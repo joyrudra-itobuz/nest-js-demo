4d9ae9e521e50c101263010fc07c3598e426396b
Joyrudra Biswas <joyrudra@itobuz.com>
2024-07-11 14:47:57 +0530

    feat : made a rest api and configured mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,20 @@ import config from './config/config';
 import { CatsModule } from './cat/cats.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(config.MONGO_URI), CatsModule],
+  imports: [
+    MongooseModule.forRoot(config.MONGO_URI, {
+      connectionFactory: (connection) => {
+        connection.once('open', () => {
+          console.log('MongoDB connection established');
+        });
+        connection.on('error', (err) => {
+          console.error('MongoDB connection error:', err);
+        });
+        return connection;
+      },
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
